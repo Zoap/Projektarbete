b2c43d00c4dddc5da861bdece5f66779a834e8ad
Zoap <tobias.wedelin@live.se>
2016-10-18 12:59:16 +0200

    Felhantering gällande användarnamn, får endast innehålla specifika karaktärer, användarnamnet får inte finnas i databasen innan registrering

--- WebSites/website2/Default.aspx.cs
+++ WebSites/website2/Default.aspx.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -34,11 +35,28 @@ public partial class _Default : System.Web.UI.Page
         {
             Server.Transfer("LoggedIN.aspx", true);
         }
-        else
-        {
-            Server.Transfer("Registration.aspx", true);
-        }
     }
+
+    private bool checkUsername(string un)
+    {
+        bool check = true;
+        byte[] ASCIIValues = Encoding.ASCII.GetBytes(un);
+        for (int i = 0; i < ASCIIValues.Length;)
+        {
+            if ((ASCIIValues[i] <= 57 && ASCIIValues[i] >= 48) || (ASCIIValues[i] <= 90 && ASCIIValues[i] >= 65) || (ASCIIValues[i] <= 122 && ASCIIValues[i] >= 97))
+            {
+            }
+            else
+            {
+                check = false;
+                break;
+            }
+            i++;
+        }
+        return check;
+    }
+
+
     protected void btnRegistration_Click(object sender, EventArgs e)
     {
         registrationSuccess.Visible = false;
@@ -51,9 +69,22 @@ public partial class _Default : System.Web.UI.Page
         string password = registrationPassword.Text;
         string passwordRepeat = registrationPasswordRepeat.Text;
 
+
+
         //Lite fenhantering (borde kollas efter specifika chars osv.) orkarde inte regex
         if (username != "")
-            if (password == passwordRepeat && password != "")
+        {
+            if (!checkUsername(username))
+            {
+                registrationError.Text = "*Användarnamnet får endast innehålla karaktärerna 0-9, A-Z, a-z";
+                registrationError.Visible = true;
+            }
+            else if (!sql.checkDuplicate(username))
+            {
+                registrationError.Text = "*Användarnamnet är upptaget";
+                registrationError.Visible = true;
+            }
+            else if (password == passwordRepeat && password != "")
             {
                 regSuccess = sql.Register(username, password);
                 if (!regSuccess)
@@ -64,6 +95,7 @@ public partial class _Default : System.Web.UI.Page
                 registrationError.Text = "*Lösenordet måste matcha";
                 registrationError.Visible = true;
             }
+        }
         else
         {
             registrationError.Text = "*Användarnamnet är inte giltigt.";
